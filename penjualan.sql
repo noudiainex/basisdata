6c2ac5b8801f49bb3e3f59ee14f53b8bdcc47743
noudiainex <148020907+noudiainex@users.noreply.github.com>
2023-11-13 14:28:25 +0900

    Add files via upload

---
+++ penjualan.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Nov 2023 pada 05.07
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `kode_brg` varchar(50) NOT NULL,
+  `nama_brg` varchar(50) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `customer`
+--
+
+CREATE TABLE `customer` (
+  `id_customer` varchar(50) NOT NULL,
+  `nama_customer` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_penjualan_barang`
+--
+
+CREATE TABLE `detail_penjualan_barang` (
+  `no_jual` varchar(50) DEFAULT NULL,
+  `kode_brg` varchar(50) DEFAULT NULL,
+  `qty_penjualan` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan_barang`
+--
+
+CREATE TABLE `penjualan_barang` (
+  `no_jual` varchar(50) NOT NULL,
+  `tgl_jual` date DEFAULT NULL,
+  `Id_customer` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`kode_brg`);
+
+--
+-- Indeks untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id_customer`);
+
+--
+-- Indeks untuk tabel `detail_penjualan_barang`
+--
+ALTER TABLE `detail_penjualan_barang`
+  ADD KEY `no_jual` (`no_jual`),
+  ADD KEY `kode_brg` (`kode_brg`);
+
+--
+-- Indeks untuk tabel `penjualan_barang`
+--
+ALTER TABLE `penjualan_barang`
+  ADD PRIMARY KEY (`no_jual`),
+  ADD KEY `Id_customer` (`Id_customer`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detail_penjualan_barang`
+--
+ALTER TABLE `detail_penjualan_barang`
+  ADD CONSTRAINT `detail_penjualan_barang_ibfk_1` FOREIGN KEY (`kode_brg`) REFERENCES `barang` (`kode_brg`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_penjualan_barang_ibfk_2` FOREIGN KEY (`no_jual`) REFERENCES `penjualan_barang` (`no_jual`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `penjualan_barang`
+--
+ALTER TABLE `penjualan_barang`
+  ADD CONSTRAINT `penjualan_barang_ibfk_1` FOREIGN KEY (`Id_customer`) REFERENCES `customer` (`id_customer`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
